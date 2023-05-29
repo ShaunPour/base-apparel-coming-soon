6dcaa578be5dba6a3405083d261d53a24455589e
Shaun <scpour@gmail.com>
2023-05-29 10:59:07 -0400

    Started working on javascript file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+const email = document.querySelector('.email-input');
+const submit = document.querySelector('.submit');
+const errorMessage = document.querySelector('.error-msg');
+const errorIcon = document.querySelector('.error-img');
+
+form.addEventListener('onsubmit', (e) => {
+    e.preventDefault();
+
+    
+})
