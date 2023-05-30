44536b03d02c55fe90a5056ea3b09d1879682891
Shaun <scpour@gmail.com>
2023-05-30 09:03:52 -0400

    Fixed javascript to show and hide error elements when the requisite conditions are met

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 const form = document.querySelector('form');
 const email = document.querySelector('.email-input');
 const submit = document.querySelector('.submit');
-const errorMessage = document.querySelector('.error-msg');
-const errorIcon = document.querySelector('.error-img');
+const error = document.querySelector('.error-div');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if(email.value.includes('@')) {
-        errorIcon.classList.remove('show');
-        errorMessage.classList.remove('show');
-    } else {
-        errorIcon.classList.add('show');
-        errorMessage.classList.add('show');
-    }
 
-})
+});
+
+submit.addEventListener('click', () => {
+    if(email.value.includes('@')) {
+        error.classList.remove('show');
+        email.classList.remove('border-error');
+    } else {
+        error.classList.add('show');
+        email.classList.add('border-error');
+    }
+});
