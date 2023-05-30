d02213ab7b3751c27156f751103def01de680654
Shaun <scpour@gmail.com>
2023-05-29 22:51:18 -0400

    Got error icon to start displaying when an email address without the @ symbol is input into the email field

--- js/app.js
+++ js/app.js
@@ -4,8 +4,14 @@ const submit = document.querySelector('.submit');
 const errorMessage = document.querySelector('.error-msg');
 const errorIcon = document.querySelector('.error-img');
 
-form.addEventListener('onsubmit', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
+    if(email.value.includes('@')) {
+        errorIcon.classList.remove('show');
+        errorMessage.classList.remove('show');
+    } else {
+        errorIcon.classList.add('show');
+        errorMessage.classList.add('show');
+    }
 
-    
 })
